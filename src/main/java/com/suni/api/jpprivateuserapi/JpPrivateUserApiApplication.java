ccd701913ca18d4cb130a7b87ce48b2ea6c83d25
SoonHyun <suni@team-mono.com>
2025-01-03 23:35:39 +0900

    feat : project build file add

---
+++ src/main/java/com/suni/api/jpprivateuserapi/JpPrivateUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.suni.api.jpprivateuserapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpPrivateUserApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpPrivateUserApiApplication.class, args);
+    }
+
+}
